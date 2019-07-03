967773f63f8b8a11a802e22e4cdb65e646feeab8
jbaglione <jbaglione@paramedic.com.ar>
2019-07-03 08:54:36 -0300

    first commit

---
+++ AmericaGisRest/Controllers/AmericaGisController.cs
@@ -0,0 +1,193 @@
+﻿using NLog;
+using GenericRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GenericRest.Controllers
+{
+    //[Authorize]
+    public class AmericaGisController : ApiController
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/AmericaGis/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        [Route("api/AmericaGis")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]RequestAmericaGis value)
+        {
+            try
+            {
+                //value = null;
+                logger.Info("Datos recibidos -> Post([FromBody]string value) " + value.ToString());
+                RequestAmericaGis item = value;// Newtonsoft.Json.JsonConvert.DeserializeObject<Request>(value);
+                logger.Info("Serialize Datos recibidos -> { JsonConvert.SerializeObject(item) " + value.ToString());
+                logger.Info("Resultado -> " + InsertValues(item));
+                return Ok("Salio OK");
+            }
+            catch (Exception ex)
+            {
+                logger.Info("Error -> " + ex.Message);
+                throw;
+            }
+        }
+
+        // PUT api/AmericaGis/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/AmericaGis/5
+        public void Delete(int id)
+        {
+        }
+
+        private static string InsertValues(RequestAmericaGis request)
+        {
+            try
+            {
+                string connetionString = null;
+                SqlConnection connection;
+                SqlDataAdapter adapter;
+                SqlCommand command = new SqlCommand();
+                SqlParameter param;
+                SqlParameter paramReturnErrorCode;
+                SqlParameter paramReturnErrorMessage;
+                string strResponse;
+                string longitud;
+                string latitud;
+
+                ConnectionStringSettings mySetting = ConfigurationManager.ConnectionStrings["cnnAmericaGis"];
+                if (mySetting == null || string.IsNullOrEmpty(mySetting.ConnectionString))
+                    throw new Exception("Fatal error: missing connecting string in web.config file");
+                connetionString = mySetting.ConnectionString;
+
+                connection = new SqlConnection(connetionString);
+
+                connection.Open();
+                command.Connection = connection;
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_AddGpsHistoricoMovilesActuales";
+
+                param = new SqlParameter("@patente", request.licensePlate??"");
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.String;
+                command.Parameters.Add(param);
+
+                if (!request.longitude.ToString().Contains(InitSepDecimal()))
+                {
+                    longitud = request.longitude.ToString().Replace(OtroSepDecimal(), InitSepDecimal());
+                }
+                else
+                {
+                    longitud = request.longitude.ToString();
+                }
+                if (!request.latitude.ToString().Contains(InitSepDecimal()))
+                {
+                    latitud = request.latitude.ToString().Replace(OtroSepDecimal(), InitSepDecimal());
+                }
+                else
+                {
+                    latitud = request.latitude.ToString();
+                }
+
+                param = new SqlParameter("@latitud", Convert.ToDecimal(latitud));
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.Decimal;
+                command.Parameters.Add(param);
+
+                param = new SqlParameter("@longitud", Convert.ToDecimal(longitud));
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.Decimal;
+                command.Parameters.Add(param);
+
+                param = new SqlParameter("@fecHorTransmision", request.reportDate);
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.DateTime;
+                command.Parameters.Add(param);
+
+                paramReturnErrorCode = new SqlParameter("@errorCode", " ");
+                paramReturnErrorCode.Direction = ParameterDirection.Output;
+                paramReturnErrorCode.DbType = DbType.String;
+                command.Parameters.Add(paramReturnErrorCode);
+
+                paramReturnErrorMessage = new SqlParameter("@errorMessage", SqlDbType.VarChar, 100);
+                paramReturnErrorMessage.Direction = ParameterDirection.Output;
+                paramReturnErrorMessage.DbType = DbType.String;
+                command.Parameters.Add(paramReturnErrorMessage);
+
+                command.ExecuteNonQuery();
+                adapter = new SqlDataAdapter(command);
+
+                if (paramReturnErrorCode.Value.ToString() == string.Empty)
+                {
+                    strResponse = "Se inserto el registro correactamente: " + request.licensePlate;
+                }
+                else
+                {
+                    strResponse = "Se inserto el registro con error: " + request.licensePlate + " - " + paramReturnErrorMessage.Value.ToString();
+                }
+
+                connection.Close();
+                return strResponse;
+
+
+            }
+            catch (Exception e)
+            {
+
+                logger.Error(e);
+                return e.Message;
+            }
+        }
+
+        private static string InitSepDecimal()
+        {
+            string wSepDecimal;
+
+            if (Convert.ToDecimal("2.5") > 3)
+                wSepDecimal = ",";
+            else
+                wSepDecimal = ".";
+
+
+            return wSepDecimal;
+
+        }
+
+        private static string OtroSepDecimal()
+        {
+            string wSepDecimal;
+
+            if (Convert.ToDecimal("2.5") > 3)
+                wSepDecimal = ".";
+            else
+                wSepDecimal = ",";
+
+
+            return wSepDecimal;
+
+        }
+    }
+}
---
+++ AmericaGisRest/Models/RequestAmericaGis.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenericRest.Models
+{
+    public class RequestAmericaGis
+    {
+        ///contiene el dominio con el que está registrado el vehículo en AmericaGis
+        public string licensePlate { get; set; } //"EZW489"
+        public string vehicleBrand { get; set; }//"Fiat"
+        public string vehicleModel { get; set; }//"Uno"
+        public string driver { get; set; }//"Diaz, Nicolas"
+        public string reportDate { get; set; }//"2019-06-12 09:23:28",
+        public int eventId { get; set; }//8,
+        public string eventName { get; set; }//"Centinela",
+        public double latitude { get; set; }//-32.403200,
+        public double longitude { get; set; }//-63.242250,
+        public decimal speed { get; set; }//0,
+        public int course { get; set; }//161,
+        public string address { get; set; }//"Salta y Boulevar España",
+        public string city { get; set; }//"Villa María",
+        public string province { get; set; }//"Córdoba"
+
+        // {
+        //    "licensePlate":"EZW489",
+        //     "vehicleBrand":"Fiat",
+        //     "vehicleModel":"Uno",
+        //     "driver":"Diaz, Nicolas",
+        //     "reportDate":"2019-06-12 09:23:28",
+        //     "eventId":8,
+        //     "eventName":"Centinela",
+        //     "latitude":-32.403200,
+        //     "longitude":-63.242250,
+        //     "speed":0,
+        //     "course":161,
+        //     "address":"Salta y Boulevar España",
+        //      "city":"Villa María",
+        //     "province":"Córdoba"
+        //}
+        //licensePlate: contiene el dominio con el que está registrado el vehículo en AmericaGis
+        //vehicleBrand: marca del vehículo
+        //vehicleModel: modelo del vehículo
+        //driver: chofer que posee asignado el vehículo al momento de enviar el reporte
+        //reportDate: fecha en que el vehículo generó el reporte
+        //eventId: valor constante que identifica el ID del evento en el sistema de AmericaGis
+        //eventName: valor constante que identifica el nombre del evento en el sistema de AmericaGis
+        //latitude: latitud del reporte
+        //longitude: longitud del reporte
+        //speed: velocidad en el momento que se generó el reporte
+        //course: orientación cardinal del vehículo, expresado en grados
+        //address: dirección georeferenciada
+        //city: ciudad georeferenciada
+        //province: provincia georeferenciada
+    }
+}
